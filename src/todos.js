e8909c3eef9f9d904c84d78a730486bb4c03f8ca
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-26 16:06:17 +0200

    create functions to add data and retrieve data from local storage

--- src/todos.js
+++ src/todos.js
@@ -16,6 +16,29 @@ export default (function Tasks() {
     console.log(container);
   };
 
+  const _addDataToStorage = (task) => {
+    let dataFromStorage;
+
+    if (localStorage.getItem("tasksArray") === null) {
+      dataFromStorage = [];
+    } else {
+      dataFromStorage = JSON.parse(localStorage.getItem("tasksArray"));
+    }
+    dataFromStorage.push(task);
+    localStorage.setItem("tasksArray", JSON.stringify(dataFromStorage));
+  };
+
+  const _getDataFromStorage = () => {
+    let dataFromStorage;
+
+    if (localStorage.getItem("tasksArray") === null) {
+      dataFromStorage = [];
+    } else {
+      dataFromStorage = JSON.parse(localStorage.getItem("tasksArray"));
+    }
+    return dataFromStorage;
+  };
+
   return {
     render,
   };
