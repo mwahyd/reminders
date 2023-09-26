945033499fed97de2fe4d63f1bd57b18e0e3684f
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-26 11:34:23 +0200

    remove getData code

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 // import style from "./style.css";
 import todoForm from "./todo-form.js";
 
-console.log(todoForm.getData());
 document.addEventListener("DOMContentLoaded", () => {
   // add a form to add tasks
   // when add task button clicked, display form
