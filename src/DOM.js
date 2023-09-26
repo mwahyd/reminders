f0db1baf0b98d4d19ff6f257bbaf0d262dcfc8c7
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-26 10:53:10 +0200

    get the form buttons; announce to subscribers when #save-btn and #cancel-btn are clicked

--- src/DOM.js
+++ src/DOM.js
@@ -13,18 +13,42 @@ export const DOM = {
     this.content = this.docu.querySelector("#content");
     this.sidebar = this.docu.querySelector("#sidebar");
     this.addTaskBtn = this.docu.querySelector("#add-btn");
+    this.saveFormBtn = this.docu.querySelector("#save-btn");
+    this.cancelFormBtn = this.docu.querySelector("#cancel-btn");
   },
 
   bindEvents: function () {
-    this.addTaskBtn.addEventListener("click", this.onClickHandler.bind(this));
+    this.addTaskBtn.addEventListener("click", this.onAddBtnClicked.bind(this));
+    this.saveFormBtn.addEventListener(
+      "click",
+      this.onFormBtnClicked.bind(this)
+    );
+    this.cancelFormBtn.addEventListener(
+      "click",
+      this.onFormBtnClicked.bind(this)
+    );
   },
 
   // handler functions
-  onClickHandler: function (event) {
+  onAddBtnClicked: function (event) {
     event.preventDefault();
     console.log(event.target);
     // announce the button is clicked to subscribers
     pubsub.publish("addTaskClicked", event.target);
+  },
+
+  onFormBtnClicked: function (event) {
+    event.preventDefault();
+    console.log(event.target);
+    switch (event.target.id) {
+      case "save-btn":
+        // announce the save button clicked to subscribers
+        pubsub.publish("formSaveBtnClicked", event.target);
+        break;
+      case "cancel-btn":
+        pubsub.publish("formCancelBtnClicked", event.target);
+        break;
+    }
   },
 
   // support functions
