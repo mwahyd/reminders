56e77430cdc2be772d7a368f2117a22be5a6bbc6
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-26 11:04:03 +0200

    save addBtn; listen to formCancelBtnClicked event; show add btn and hide form

--- src/todo-form.js
+++ src/todo-form.js
@@ -2,18 +2,27 @@ import { DOM } from "./DOM.js";
 import { pubsub } from "./pubsub.js";
 
 export default (function TodoForm() {
+  let addBtn;
   const getData = function () {
     return DOM;
   };
 
   const render = function () {
     // ask pubsub controller to notify when addTaskClicked event occurs
-    pubsub.subscribe("addTaskClicked", _showForm);
+    pubsub.subscribe("addTaskClicked", _showFormHideButton);
+    pubsub.subscribe("formCancelBtnClicked", _showBtnHideForm);
   };
 
-  const _showForm = function (button) {
-    console.log(DOM.mainContainer);
-    console.log(button);
+  const _showFormHideButton = function (button) {
+    console.log(DOM.mainContainer.querySelector("#form"));
+    DOM.mainContainer.querySelector("#form").classList.remove("hidden");
+    button.classList.add("hidden");
+    addBtn = button;
+  };
+
+  const _showBtnHideForm = function (data) {
+    DOM.mainContainer.querySelector("#form").classList.add("hidden");
+    addBtn.classList.remove("hidden");
   };
 
   return {
