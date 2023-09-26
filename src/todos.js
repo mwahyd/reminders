a59de6f5bbf18751b1ccfe1c0f7e85428c2fc58f
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-26 15:59:58 +0200

    create Tasks IIFE; add a render() that listens to newTaskCreated event and call _saveTask()

--- src/todos.js
+++ src/todos.js
@@ -1,1 +1,22 @@
 import { pubsub } from "./pubsub.js";
+
+export default (function Tasks() {
+  const render = () => {
+    // listen to a new task created event
+    pubsub.subscribe("newTaskCreated", _saveTask);
+
+    // display task cards if stored in local storage
+  };
+
+  const _saveTask = ([data, container]) => {
+    // create task ID, (use length of array in local storage)
+    // create date stamp (date of creation)
+    // store task in local storage
+    console.log(data);
+    console.log(container);
+  };
+
+  return {
+    render,
+  };
+})();
