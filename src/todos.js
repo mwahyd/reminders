2227565aeaf95f7f97f4d4876e1bf3c42910737b
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-27 16:48:55 +0200

    create a task card for each task; call _createTaskCards() when new task saved; create a function that dynamically adds the HTML structure

--- src/todos.js
+++ src/todos.js
@@ -4,11 +4,10 @@ export default (function Tasks() {
   const render = () => {
     // listen to a new task created event
     pubsub.subscribe("newTaskCreated", _saveTask);
-    // // listen to a new task being saved in storage
-    // pubsub.subscribe("newTaskSavedInStorage", _createTaskCard);
 
     // display task cards if stored in local storage
-    _createTaskCards();
+    // ! should not call document here ...
+    _createTaskCards(document.querySelector("#content"));
   };
 
   const _saveTask = ([data, container]) => {
@@ -20,7 +19,6 @@ export default (function Tasks() {
     const st = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
     // merge all the objects from data into one object
-
     data = Object.assign({}, ...data);
     data["taskID"] = taskID;
     data["createdDate"] = st;
@@ -28,16 +26,28 @@ export default (function Tasks() {
     // store task in local storage
     _addDataToStorage(data);
 
+    // call task cards render to update DOM
+    _createTaskCards(container);
+
     console.log(data);
     console.log(container);
   };
 
-  const _createTaskCards = () => {
+  const _createTaskCards = (contentDiv) => {
     // listen for a task saved in storage
     // get data from storage
     const tasks = _getDataFromStorage();
-    console.log(tasks);
     // if empty display none; else display on DOM
+    if (tasks.length === 0) {
+      console.log("array empty");
+      return;
+    }
+    console.log(tasks);
+    contentDiv.lastElementChild.innerHTML = "";
+    tasks.forEach((task) => {
+      const elements = _createTaskElements(task);
+      contentDiv.lastElementChild.appendChild(elements);
+    });
 
     // create a task card for each item stored in storage
   };
@@ -57,6 +67,25 @@ export default (function Tasks() {
       dataFromStorage = JSON.parse(localStorage.getItem("tasksArray"));
     }
     return dataFromStorage;
+  };
+
+  // _createTaskCards();
+  const _createTaskElements = (obj) => {
+    const container = document.createElement("div");
+    container.setAttribute("data-index", obj["taskID"]);
+    container.classList.add("red-border");
+    container.innerHTML = `
+      <div class="task-header flex-sb">
+        <p>taskID: <span>${obj["taskID"]}</span></p>
+        <p>issued: <span>${obj["createdDate"]}</span>
+      </div>
+
+      <div id="task-priority"><p>priority: <span>${obj["priority"]}</span></p></div>
+      <div id="task-title"><p>title: <span class="bold">${obj["title"]}</span></p></div>
+      <div id="task-description"><p>description: <span>${obj["description"]}</span></p></div>
+      <div id="task-due"><p>due: <span>${obj["dueDate"]}</span></p></div>
+    `;
+    return container;
   };
 
   return {
