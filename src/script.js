e7cc418652fbc5ed904cbe1ad3307526d2f33a06
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-27 15:13:46 +0200

    call Tasks.render()

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
 // import style from "./style.css";
-import todoForm from "./todo-form.js";
+import TodoForm from "./todo-form.js";
+import Tasks from "./todos.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // add a form to add tasks
   // when add task button clicked, display form
-  todoForm.render();
+  TodoForm.render();
+
+  // when a new task is created; store the task
+  // when page loads, display tasks from storage
+  Tasks.render();
 });
