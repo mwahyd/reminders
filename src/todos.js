9b40c5a5eb1932d2d9eeb9f869a3d9e47400d5f5
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-28 13:23:57 +0200

    refactor _delBtnClicked()

--- src/todos.js
+++ src/todos.js
@@ -68,9 +68,7 @@ export default (function Tasks() {
   };
 
   const _delBtnClicked = (btn, contentDiv) => {
-    if (!confirm("Are you sure you want to remove this item?")) {
-      return;
-    }
+    if (!confirm("Are you sure you want to remove this item?")) return;
     _removeItemFromArray(btn);
     _createTaskCards(contentDiv);
   };
