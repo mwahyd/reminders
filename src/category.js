4831e89a51f6ab0ff98c9da1a5afcb6105d7f559
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-10-02 11:55:00 +0200

    refactor code; create handler function that calls _displayPriority with priority level

--- src/category.js
+++ src/category.js
@@ -4,7 +4,8 @@ import Tasks from "./todos.js";
 export default (function Category() {
   const render = () => {
     // listen to each category event and fire associated func
-    pubsub.subscribe("priorityLowClicked", _displayLow);
+    pubsub.subscribe("priorityClicked", _displayPriorityHandler);
+    pubsub.subscribe("allClicked", _displayAllTasks);
   };
 
   const _renderTasks = (tasks, contentDiv) => {
@@ -15,10 +16,31 @@ export default (function Category() {
     });
   };
 
-  const _displayLow = ([priorityLow, contentDiv]) => {
-    console.log("prority: low clicked");
+  const _displayAllTasks = (allButton) => {
+    console.log(allButton);
+    // display all the tasks from other categories as well
+    document.location.reload();
+  };
+
+  const _displayPriorityHandler = ([priority, contentDiv]) => {
+    console.log(priority);
+    switch (priority.id) {
+      case "priority-low":
+        _displayPriority("low", contentDiv);
+        break;
+      case "priority-medium":
+        _displayPriority("medium", contentDiv);
+        break;
+      case "priority-high":
+        _displayPriority("high", contentDiv);
+        break;
+    }
+  };
+
+  const _displayPriority = (priority, contentDiv) => {
+    console.log(`prority: ${priority} clicked`);
     const allTasks = Tasks.getDataFromStorage();
-    const lowTasks = allTasks.filter((obj) => obj["priority"] === "low");
+    const lowTasks = allTasks.filter((obj) => obj["priority"] === priority);
     _renderTasks(lowTasks, contentDiv);
   };
 
