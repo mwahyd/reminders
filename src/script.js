2b1ad763b8c81857d76cade340e478d0eca34be9
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-10-12 15:49:38 +0200

    import category & invoke render

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 // import style from "./style.css";
 import TodoForm from "./todo-form.js";
 import Tasks from "./todos.js";
+import Category from "./category.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // add a form to add tasks
@@ -10,4 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // when a new task is created; store the task
   // when page loads, display tasks from storage
   Tasks.render();
+
+  // display tasks in their own categories
+  Category.render();
 });
